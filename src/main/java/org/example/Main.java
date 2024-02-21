a1121f8e364e1b154a59df18c630783357a1bc45
Ayush Jain <ayushganna67@gmail.com>
2024-02-21 14:10:34 +0530

    Add files - constructor, this and static

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,15 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome to Target!");
+        Student student1 = new Student("Ayush", 20, true);
+        Student student2 = new Student("Sanidhya");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Student2 age - " + student2.age);
+
+        //Static keyword
+        System.out.println("Student college - "  + Student.college);
+        int sum = Utils.add(student1.age, student2.age);
+        System.out.println("Using static method from Util = " + sum);
     }
 }
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class Student {
+    String name;
+    int age;
+    boolean isEligible;
+    static String college = "RIT";
+
+    //parameterized constructor
+    public Student(String name, int age, boolean isEligible) {
+        //this(name, age);
+        this.name = name;
+        this.age = age;
+        this.isEligible = isEligible;
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.isEligible = true;
+    }
+
+    public Student(String name) {
+        this.name = name;
+        this.age = 21;
+        this.isEligible = true;
+    }
+
+    public Student(){
+        this.name = "";
+        this.age = 18;
+        this.isEligible = true;
+    }
+}
