8958017c99b355f219403b4f9b1a85c4ddc89296
Ayush Jain <ayushganna67@gmail.com>
2024-02-22 15:03:09 +0530

    Add files - Thread pool

---
+++ src/main/java/Lesson4/Worker.java
@@ -0,0 +1,17 @@
+package Lesson4;
+
+public class Worker extends Thread{
+    String message;
+    public Worker(String s){
+        this.message=s;
+    }
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+" (Start) message = "+message);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(Thread.currentThread().getName()+" (End)");//prints thread name
+    }
+}
