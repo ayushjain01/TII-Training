225351da7479acc680fbcde738afa9315f68815d
Ayush Jain <ayushganna67@gmail.com>
2024-02-23 12:17:57 +0530

    Add files - Collections - Sort student array list

---
+++ src/main/java/Lesson5/StudentArrayList.java
@@ -0,0 +1,32 @@
+package Lesson5;
+import java.util.*;
+
+public class StudentArrayList {
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("ayush", 21));
+        students.add(new Student("sanidhya", 21));
+        students.add(new Student("aman", 12));
+        students.add(new Student("adeeb", 21));
+        students.sort(Comparator.comparingInt(Student::getAge)
+                .thenComparing(Student::getName));
+        for(Student a: students){
+            System.out.println("Name: " + a.getName() + " Age: " + a.getAge());
+        }
+    }
+}
+class Student {
+    String name;
+    int age;
+    public Student(String name,  int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    int getAge() {
+        return this.age;
+    }
+    String getName() {
+        return this.name;
+    }
+}
